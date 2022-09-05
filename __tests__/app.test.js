50f393557f62c84554f22ce282ade510cbddcc44
gcltukjs2022 <gcltukjs2022@gmail.com>
2022-09-05 14:52:10 +0100

    create getArticleById and tests, add error handling and  modify README

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,3 +37,41 @@ describe("GET", () => {
       });
   });
 });
+
+describe("GET", () => {
+  test("200: gets article by id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.article).toBe("object");
+        expect(Object.keys(body.article).length).toBe(7);
+
+        expect(body.article).toMatchObject({
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: expect.any(Number),
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+        });
+      });
+  });
+  test("400: article_id not exists", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("No Such Article");
+      });
+  });
+  test("404: API does not exist", () => {
+    return request(app)
+      .get("/api/not_an_valid_path/1")
+      .expect(404)
+      .then((response) => {
+        expect(response.res.statusMessage).toBe("Not Found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const { getArticleById } = require("./controllers/articles.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal server error" });
+});
 
 module.exports = app;
